b603a98d2f64c107c24336a2a30f7f9ecd2c182b
Max Barrett <maxjbarrett@gmail.com>
2015-03-11 14:15:11 +1100

    Auto reference built files in minified html

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,20 @@ var ghPages = require('gulp-gh-pages');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
 var minifyHTML = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
 var header = require('gulp-header');
 var pkg = require('./package.json');
 var info = '// <%= pkg.name %>@v<%= pkg.version %>, <%= pkg.license %>\n';
 
+
 // Delete existing dist folder
-gulp.task('clean', require('del').bind(null, ['dist']));
+gulp.task('clean', require('del').bind(null, ['./dist']));
 
 
-// Lint Task
+// Lint the JS
 gulp.task('jshint', function () {
   return gulp.src('js/**/*.js')
     .pipe(jshint())
@@ -24,29 +28,18 @@ gulp.task('jshint', function () {
 });
 
 
-// Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(rename(function (path) {
-        	path.basename += "-min";
-    	}))
-        .pipe(uglify())
-        .pipe(header(info, { pkg : pkg }))
-        .pipe(gulp.dest('./dist/js'));
-});
+// mush the files
+gulp.task('html', function () {
+  var assets = useref.assets({searchPath: ['app', '.']});
 
-
-// Minify HTML
-gulp.task('minify-html', function() {
-  var opts = {
-    conditionals: true,
-    spare:true,
-    quotes:true
-  };
- 
-  return gulp.src('./*.html')
-    .pipe(minifyHTML(opts))
-    .pipe(gulp.dest('./dist/'));
+  return gulp.src('./*.html') // grab html file/s
+    .pipe(assets)
+    .pipe(gulpif('*.js', uglify())) // minify any js files
+    .pipe(gulpif('*.css', minifyCss())) // minify css
+    .pipe(assets.restore())
+    .pipe(useref()) // rename built/minified file refs in html
+    .pipe(gulpif('*.html', minifyHTML({conditionals: true, spare: true, quotes:true}))) // minify html
+    .pipe(gulp.dest('./dist/')); // pipe into /dist folder
 });
 
 
@@ -57,11 +50,13 @@ gulp.task('deploy', function() {
 });
 
 
-gulp.task('build', ['jshint', 'scripts', 'minify-html'], function () {
+// do the build
+gulp.task('build', ['jshint', 'html'], function () {
  	return gulp.src('./dist/**/*');
 });
 
 
+// default task: clean up and then build
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
